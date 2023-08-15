e6274ed80bcf7694ccfa09b8dcb5b80f97f71568
Danial Klymenko <danilsmanagement@gmail.com>
2023-08-16 02:44:16 +0300

    Added Navbar & Google Auth & connected MongoDB

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,8 @@
 import '@styles/globals.css';
 
+import { Nav } from '@components/Nav';
+import { Provider } from '@components/Provider';
+
 export const metadata = {
   title: 'PromptAI',
   description: 'Discover and Share AI Prompts',
@@ -8,12 +11,16 @@ export const metadata = {
 const RootLayout = ({ children }) => {
   return (
     <html lang="eng">
-      <body>
-        <div className="main">
-          <div className="gradient"></div>
-        </div>
-
-        <main className="app">{children}</main>
+      <body suppressHydrationWarning={true}>
+        <Provider>
+          <div className="main">
+            <div className="gradient"></div>
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,21 @@
+import { Feed } from '@components/Feed';
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover and Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        PrompedAI is a website where you can find, create, and share AI prompts. It helps you explore ideas for various
+        things using the power of AI.
+      </p>
+
+      <Feed />
+    </section>
+  );
 };
 
 export default Home;
