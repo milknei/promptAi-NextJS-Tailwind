58858be9b42b5c9b31f1d99c8d7e423ada337394
Danial Klymenko <danilsmanagement@gmail.com>
2023-08-16 23:05:12 +0300

    Added feed

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,7 +1,55 @@
-import React from 'react'
+'use client';
 
-export const PromptCard = () => {
+import { useState } from 'react';
+import Image from 'next/image';
+import { useSession } from 'next-auth/react';
+import { usePathname, useRouter } from 'next/navigation';
+
+export const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+  const router = useRouter;
+
+  const [copied, setCopied] = useState('');
+
+  const handleCopy = () => {
+    setCopied(post.prompt);
+    navigator.clipboard.writeText(post.prompt);
+    setTimeout(() => setCopied(''), 3000);
+  };
+
   return (
-    <div>PromptCard</div>
-  )
-}
+    <div className="prompt_card">
+      <div className="flex justify-between items-start gap-5">
+        <div className="flex flex-1 justify-start items-center gap-3 cursor-pointer">
+          <Image
+            src={post.creator.image}
+            alt="user image"
+            width={40}
+            height={40}
+            className="rounded-full object-contain"
+          />
+          <div className="flex flex-col">
+            <h3 className="font-satoshi font-semibold text-gray-900">{post.creator.username}</h3>
+            <p className="font-inter text-sm text-gray-500">{post.creator.email}</p>
+          </div>
+        </div>
+
+        <div className="copy_btn" onClick={() => handleCopy()}>
+          <Image
+            width={15}
+            height={15}
+            src={copied === post.prompt ? '/assets/icons/tick.svg' : '/assets/icons/copy.svg'}
+            alt={copied === post.prompt ? 'Prompt copied' : 'Copy prompt'}
+          />
+        </div>
+      </div>
+
+      <p className="my-4 font-satoshi text-sm text-gray-700">{post.prompt}</p>
+      <p
+        className="font-inter text-sm blue_gradient cursor-pointer"
+        onClick={() => handleTagClick && handleTagClick(post.tag)}
+      >
+        {post.tag}
+      </p>
+    </div>
+  );
+};
