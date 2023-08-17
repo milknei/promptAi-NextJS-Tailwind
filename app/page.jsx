9a77859a9f1a64b91e4184775554a687eeafb53a
Danial Klymenko <danilsmanagement@gmail.com>
2023-08-17 22:27:14 +0300

    Design fixes

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <section className="w-full flex-center flex-col">
       <h1 className="head_text text-center">
         Discover and Share
-        <br className="max-md:hidden" />
+        <br />
         <span className="orange_gradient text-center">AI-Powered Prompts</span>
       </h1>
       <p className="desc text-center">
